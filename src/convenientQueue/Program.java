b8a8df421b0b67631823c63cae017447e4077d2c
malabarMCB <grinevich97@gmail.com>
2018-04-30 14:54:43 +0300

    Add program

---
+++ src/convenientQueue/Program.java
@@ -0,0 +1,16 @@
+package convenientQueue;
+
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+
+public class Program extends Application {
+    @Override
+    public void start(Stage stage){
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
